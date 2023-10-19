b6962d841b3734ad9ddb2cab417088b430b6b3b3
Diog <ln.xxng@gmail.com>
2023-10-19 02:18:02 +0200

    a lot of changes + chart

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-
-
-
 const express = require('express');
 // const request = require('request'); // using 'request' npm package
 const https = require('https');
@@ -29,16 +26,24 @@ appEmotion.use('/uuid', express.static(path.join(__dirname, 'node_modules/uuid/d
 
 // Serve static files for the homepage
 appHome.use(express.static(path.join(__dirname, 'public')));
+appHome.use('/.well-known', express.static(path.join(__dirname, 'public', '.well-known')));
+
 
 // // Serve static files for the emotion page
 // appEmotion.use(express.static(path.join(__dirname, 'public/emotion')));
 
 // SSL certificate details
-const httpsOptions = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem'),
-  passphrase: 'Hidalgo'
+const privateKey = fs.readFileSync('private/private.key', 'utf8');
+const certificate = fs.readFileSync('private/certificate.crt', 'utf8');
+const ca = fs.readFileSync('private/ca_bundle.crt', 'utf8');
+
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
 };
+
 
 // Listen on port 80 for the homepage
 appHome.listen(80, () => {
@@ -46,7 +51,7 @@ appHome.listen(80, () => {
 });
 
 // Listen on port 4443 for the emotion page using HTTPS
-const server=https.createServer(httpsOptions, appEmotion).listen(portEmotion, '0.0.0.0', () => {
+const server=https.createServer(credentials, appEmotion).listen(portEmotion, '0.0.0.0', () => {
   console.log(`Server running at https://localhost:${portEmotion}/`);
 });
 
@@ -56,8 +61,13 @@ const server=https.createServer(httpsOptions, appEmotion).listen(portEmotion, '0
 
 const wss = new WebSocket.Server({ server });
 
+date=new Date().toDateString();
+const filePath = path.join(__dirname, 'data', 'emotionData'+date+'.txt');
+console.log(filePath);
+const buffer = [];
+const MAX_BUFFER_SIZE = 1000;
 
-const receiver='jupyter-xxl'
+const receiver='Hidalgo_WhoAreYou';
 
 // Object to store client WebSocket connections using clientId as key
 const clients = {};
@@ -68,27 +78,54 @@ wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     try {
       const payload = JSON.parse(message);
-      const { clientId, emotionData } = payload;
-      
+      const { clientId, emotionData,landmarks,tmpA,tmpB } = payload;
+      const dataToSend={
+        clientId,
+        emotionData
+      }
       if (clientId) {
-        // Update the clients map with the current WebSocket connection
         clients[clientId] = ws;
       }
-      console.log('Client ID:', clientId);
-      console.log('Received:', emotionData);
+      // console.log('Client ID:', clientId);
+      // console.log('Received:', emotionData);
+      payload.timestamp = new Date().toISOString();
 
-      if (receiver){
-        sendMessageToClient(receiver,message);
+      if (receiver in clients) {
+        // sendMessageToClient(receiver, "you are Paul?");
+        // console.log('found receiver');
+        if (emotionData && Object.keys(emotionData).length > 0) {
+          sendMessageToClient(receiver, JSON.stringify(dataToSend));
+        }
       }
-
-      // Process or save the emotionData as required
+      
+      buffer.push(JSON.stringify(payload));
+      if (buffer.length >= MAX_BUFFER_SIZE) {
+        const dataToWrite = buffer.join('\n') + '\n';
+        buffer.length = 0;  // clear the buffer
+        fs.appendFile(filePath, dataToWrite, (err) => {
+          if (err) {
+            console.log( err);
+          }
+          console.log('Data stored');
+        });
+      }
     } catch (error) {
       console.error("Error parsing message:", error);
     }
   });
 
+
+
+  ws.on('error', (error) => {
+    console.error("WebSocket Client Error:", error);
+  });
+
   ws.send('Welcome to the server!');
 });
+
+
+
+
 
 wss.onerror = function(error) {
   console.error("WebSocket Error:", error);
@@ -103,3 +140,28 @@ function sendMessageToClient(clientId, message) {
     console.error("Client not connected or WebSocket not in OPEN state");
   }
 }
+
+function flushBuffer() {
+  if (buffer.length > 0) {
+      const dataToWrite = buffer.join('\n') + '\n';
+      buffer.length = 0;  // clear the buffer
+      fs.appendFileSync(filePath, dataToWrite); // Use synchronous method to ensure data gets written before exit
+      console.log('Last bit of data stored');
+  }
+}
+
+// Listen to the exit event
+process.on('exit', flushBuffer);
+
+// Optionally, you can also handle other signals like SIGINT (Ctrl+C) or uncaught exceptions
+process.on('SIGINT', function() {
+  flushBuffer();
+  process.exit(0); // this is required to actually terminate the process after writing the buffer
+});
+
+process.on('uncaughtException', function(err) {
+  console.error('Caught exception:', err);
+  flushBuffer();
+  process.exit(1); // this is required to actually terminate the process after writing the buffer
+});
+
